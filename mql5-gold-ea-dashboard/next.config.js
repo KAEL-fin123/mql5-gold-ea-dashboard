03d8702b5df99ff5f046e45f41c89cbb859c7d5e
KAEL-fin123 <surongbin@hcs55.com>
2025-06-11 22:34:24 +0800

    修复: 将next.config.ts更改为next.config.js

---
+++ mql5-gold-ea-dashboard/next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
+  images: {
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+};
+
+module.exports = nextConfig; 
